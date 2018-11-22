7f333939dec75557d19d8aed0c90a0e269e64328
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2018-11-22 14:43:38 +0000

    Update Ansible.groovy

--- src/uk/gov/hmcts/Ansible.groovy
+++ src/uk/gov/hmcts/Ansible.groovy
@@ -83,7 +83,7 @@ class Ansible implements Serializable {
           export ANSIBLE_FORCE_COLOR=true
 
           ansible-playbook ${verbosestring} "${playbookName}" \
-          -i ./inventory \
+          -i 172.16.0.18 \
           --limit "${limit(environment, playbookName)}" \
           --extra-vars "deploy_target=${env(environment)}" \
           --extra-vars "{'versions': ${versions} }" \
